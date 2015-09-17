b4ec03af66491b296a25fee703e198ce0e446290
Hank Tom <hanktom@gmail.com>
2015-09-17 19:48:33 +0800

    startActivityForResult, setResult...

---
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.tom.atm;
+
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v){
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edPasswd = (EditText) findViewById(R.id.passwd);
+        String uid = edUserid.getText().toString();
+        String pw = edPasswd.getText().toString();
+        if (uid.equals("jack") && pw.equals("1234")){
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            setResult(1);
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("登入")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
