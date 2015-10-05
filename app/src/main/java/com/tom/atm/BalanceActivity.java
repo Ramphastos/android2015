df9b250742aa07fc74c9db7506184bc4a3fc954a
Hank Tom <hanktom@gmail.com>
2015-10-05 21:13:54 +0800

    餘額查詢(二)

--- app/src/main/java/com/tom/atm/BalanceActivity.java
+++ app/src/main/java/com/tom/atm/BalanceActivity.java
@@ -1,10 +1,23 @@
 package com.tom.atm;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BalanceActivity extends AppCompatActivity {
 
@@ -15,9 +28,55 @@ public class BalanceActivity extends AppCompatActivity {
         AtmApplication myapp = (AtmApplication)getApplication();
         Log.d("USERID", myapp.userid);
         Log.d("PASSWD", myapp.passwd);
+//        http://j.snpy.org/atm/b?userid=tom&pw=1234
 
-
+        String s = "http://j.snpy.org/atm/b?userid="+myapp.userid+"&pw="+myapp.passwd;
+        new BalanceTask().execute(s);
     }
+
+    class BalanceTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "";
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                /*
+                InputStream is = conn.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader in = new BufferedReader(isr);*/
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String line = in.readLine();
+                while(line!=null){
+                    result = result+line;
+                    line = in.readLine();
+                }
+                Log.d("RESULT", result);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject json = new JSONObject(s);
+                int balance = json.getInt("balance");
+                TextView tv = (TextView) findViewById(R.id.balance);
+                tv.setText("您的餘額為:"+balance);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
