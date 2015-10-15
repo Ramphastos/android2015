dbdfe1138c25c2b47c9e669c5e84933fcc541db4
Hank Tom <hanktom@gmail.com>
2015-10-15 20:59:33 +0800

    extends SimpleCursorAdapter

--- app/src/main/java/com/tom/images/MainActivity.java
+++ app/src/main/java/com/tom/images/MainActivity.java
@@ -1,15 +1,21 @@
 package com.tom.images;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,11 +38,39 @@ public class MainActivity extends AppCompatActivity {
         String[] from = {MediaStore.Images.Media.DATA, MediaStore.Images.Media.DISPLAY_NAME};
         int[] to = {R.id.img, R.id.filename};
 
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+        /*SimpleCursorAdapter adapter = new SimpleCursorAdapter(
                 this, R.layout.icon, c, from , to , 1
         );
+        */
         GridView grid = (GridView) findViewById(R.id.grid);
+        GridImageAdapter adapter = new GridImageAdapter( this, R.layout.icon, c, from , to , 1);
         grid.setAdapter(adapter);
+    }
+
+    class GridImageAdapter extends SimpleCursorAdapter{
+
+        public GridImageAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+            super(context, layout, c, from, to, flags);
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView==null){
+                View v = getLayoutInflater().inflate(R.layout.icon, null);
+                ImageView iv = (ImageView) v.findViewById(R.id.img);
+                TextView tv = (TextView) v.findViewById(R.id.filename);
+                Cursor c = getCursor();
+                c.moveToPosition(position);
+                tv.setText(c.getString(c.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)));
+                int id = c.getInt(c.getColumnIndex(MediaStore.Images.Media._ID));
+
+                iv.setImageURI(Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id+""));
+                convertView = v;
+            }
+            return convertView;
+        }
+
     }
 
     @Override
